bdd7cd7c9aa97b7c21f5ddc1241f4ff61472afa1
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-03-03 12:42:50 +0900

    [Gold IV] Title: 타임머신, Time: 324 ms, Memory: 19556 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static long[] costArr; 
+    final static long INF = 987654321;
+    
+    static class Edge {
+        int from, to, cost;
+        
+        public Edge(int from, int to, int cost){
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        costArr = new long[N+1];  
+        List<Edge> edges = new ArrayList<>();
+
+        // 값 입력 받기
+        for (int i = 0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(x, y, cost));
+        }
+
+        
+        // 벨만-포드 알고리즘
+        // 최단거리에서 음수 간선, 등으로 인하여 계속해서 최단거리를 줄여나가는지 "감지" 해야하는 경우 사용
+        Arrays.fill(costArr, INF);
+        costArr[1] = 0;
+        
+        for(int i=2; i<=N; i++){
+            for(Edge edge: edges){
+                if(costArr[edge.from] != INF && costArr[edge.to] > costArr[edge.from] + edge.cost) {
+                    costArr[edge.to] = costArr[edge.from] + edge.cost;
+                }
+            }
+        }
+        
+        boolean hasNegativeCycle = false;
+        for(Edge edge: edges){
+            if(costArr[edge.from] != INF && costArr[edge.to] > costArr[edge.from] + edge.cost) {
+                    hasNegativeCycle = true;
+                    break; 
+                }
+        }
+        
+        
+        // 출력
+        if (hasNegativeCycle) {
+            bw.write("-1");
+        } else {
+            for (int i = 2; i <= N; i++) {
+                if (costArr[i] == INF) {
+                    bw.write("-1" + " ");
+                } else {
+                    bw.write(Long.toString(costArr[i]) + " "); 
+                }
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
