22d2d2568a77eb11a87d437e2a893c547640f6de
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-03-04 09:44:41 +0900

    [Gold V] Title: CCW, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Point[] p = new Point[3];
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            p[i] = new Point(x, y);
+        }
+
+        int result = (p[0].x * p[1].y) + (p[1].x * p[2].y) + (p[2].x * p[0].y) - (p[1].x * p[0].y) - (p[2].x * p[1].y)
+                - (p[0].x * p[2].y);
+
+        if (result > 0)
+            System.out.println("1");
+        else if (result == 0)
+            System.out.println("0");
+        else
+            System.out.println("-1");
+
+    }
+}
