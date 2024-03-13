fd1ab4a6b8910e8efead2d1c3e4a8f020e27f872
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-03-13 12:22:57 +0900

    [Gold II] Title: 트리의 지름, Time: 736 ms, Memory: 91568 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {    
+
+    static ArrayList<Node>[] list;
+    static boolean[] visited;
+    static int max = 0;
+    static int node;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        list = new ArrayList[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());  // 수정: e -> s
+            while (true) {
+                int e = Integer.parseInt(st.nextToken());
+                if (e == -1) break;
+                int cost = Integer.parseInt(st.nextToken());
+                list[s].add(new Node(e, cost));
+            }
+        }
+
+        //임의의 노드(1)에서 부터 가장 먼 노드를 찾는다. 이때 찾은 노드를 node에 저장한다.
+        visited = new boolean[n + 1];
+        dfs(1, 0);
+
+        //node에서 부터 가장 먼 노트까지의 거리를 구한다.
+        visited = new boolean[n + 1];
+        dfs(node, 0);
+
+        System.out.println(max);
+    }
+
+    public static void dfs(int x, int len) {
+        if (len > max) {
+            max = len;
+            node = x;
+        }
+        visited[x] = true;
+
+        for (int i = 0; i < list[x].size(); i++) {
+            Node n = list[x].get(i);
+            if (!visited[n.n]) { 
+                dfs(n.n, n.cost + len);
+                visited[n.n] = true;
+            }
+        }
+    }
+
+    public static class Node {
+        int n; 
+        int cost;
+
+        public Node(int n, int cost) {
+            this.n = n;
+            this.cost = cost;
+        }
+    }
+}
