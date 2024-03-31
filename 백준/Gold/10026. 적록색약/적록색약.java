273782687187176e35f7030820a947f137d3a783
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-03-31 14:52:26 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 16256 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,64 @@
+import java.io.*;
+
+public class Main{
+    
+    static int n;
+    static int cnt1, cnt2;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static char[][] board1, board2;
+    static boolean[][] visited;
+    
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        board1 = new char[n][n];
+        board2 = new char[n][n];
+        visited = new boolean[n][n];
+        
+        for(int i=0; i<n; i++){
+            String st = br.readLine();
+            for(int j=0; j<n; j++){
+                board1[i][j] = st.charAt(j);
+                board2[i][j] = st.charAt(j);
+                if(board2[i][j] == 'G') board2[i][j] = 'R';
+            }
+        }
+        
+        cnt1 = countArea(board1);
+        cnt2 = countArea(board2);
+        
+        System.out.println(cnt1 + " " + cnt2);
+           
+    }
+    
+    public static int countArea(char[][] board) {
+        int count = 0;
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j, board[i][j], board);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public static void dfs(int x, int y, char color, char[][] board){
+        
+        visited[x][y] = true;
+        
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+            if(!visited[nx][ny] && board[nx][ny] == color){
+                dfs(nx, ny, color, board);
+            }
+        }
+    }     
+}
