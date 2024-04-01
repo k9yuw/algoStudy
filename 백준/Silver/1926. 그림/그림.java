ea26b652a984c7fdbc43492eae36fb8a4c4ecbb2
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-04-01 13:09:22 +0900

    [Silver I] Title: 그림, Time: 1136 ms, Memory: 48824 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int maxArea = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] board;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int areaNum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1 && !visited[i][j]) {
+                    int curArea = dfs(i, j); // 현재 영역의 넓이 구함
+                    maxArea = Math.max(maxArea, curArea);
+                    areaNum++;
+                }
+            }
+        }
+
+        System.out.println(areaNum);
+        System.out.println(maxArea);
+    }
+
+    public static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int count = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                if (board[nx][ny] == 1 && !visited[nx][ny]) {
+                    count += dfs(nx, ny);
+                }
+            }
+        }
+        return count;
+    }
+}
