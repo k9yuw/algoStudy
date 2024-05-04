18fde5f429114be442b558409994dd6d236b3422
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-05-04 23:36:49 +0900

    [Bronze III] Title: 사과, Time: 132 ms, Memory: 14252 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int total = 0;
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int student = Integer.parseInt(st.nextToken());
+            int apple = Integer.parseInt(st.nextToken());
+            total += apple % student;
+        }
+        System.out.print(total);
+    }
+}
