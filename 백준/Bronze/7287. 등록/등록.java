467f85a2449de0f2fdccf3069ce096fc91180617
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-06-15 12:42:21 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        System.out.println(174);
+        System.out.println("gyuwoneeee");
+    }
+}
