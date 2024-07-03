7325ded572fa6a2c87417d153858f83d391337ab
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-07-03 12:05:52 +0900

    [Gold V] Title: 집합의 표현, Time: 508 ms, Memory: 54808 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (command == 0) {
+                union(a, b);
+            } else if (command == 1) {
+                sb.append((isSameParent(a, b) ? "YES" : "NO") + "\n");
+            } else {
+                continue;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (x < y) parent[y] = x;
+            else parent[x] = y;
+        }
+    }
+
+    public static boolean isSameParent(int x, int y) {
+        return find(x) == find(y);
+    }
+}
