b9398e3ffb8899ccbdfb660e4a18d26499e7e062
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-07-11 00:50:27 +0900

    [Silver II] Title: 쇠막대기, Time: 164 ms, Memory: 16416 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String pipe = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+        for(int i=0; i < pipe.length(); i++) {
+            if (pipe.charAt(i) == '(') {
+                stack.push('(');
+            } else if (pipe.charAt(i) == ')') {
+                stack.pop();
+            }
+            if (i > 0) {
+                if (pipe.charAt(i - 1) == '(' && pipe.charAt(i) ==')') {
+                    result += stack.size();
+                } else if (pipe.charAt(i-1)==')' && pipe.charAt(i) ==')'){
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
