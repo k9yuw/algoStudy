b856c4d82ff801b9eff6ad1659fdfc49b2090c14
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-08-05 20:15:09 +0900

    [Gold V] Title: 집합의 표현, Time: 436 ms, Memory: 53156 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,58 +3,66 @@ import java.util.*;
 
 public class Main {
 
-    static int n, m;
-    static int[] parent;
+	static int N, M;
+	static int[] parent;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        parent = new int[n + 1];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-        }
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int command = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
 
-            if (command == 0) {
-                union(a, b);
-            } else if (command == 1) {
-                sb.append((isSameParent(a, b) ? "YES" : "NO") + "\n");
-            } else {
-                continue;
-            }
-        }
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int action = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if (action == 0) {
+				union(a, b);
+			} else if (action == 1) {
+				sb.append(isSameParent(a, b) ? "YES" : "NO");
+				sb.append("\n");					
+			}
+		}
+		
+		System.out.println(sb);
+	}
 
-        System.out.println(sb);
-    }
+	static void union(int x, int y) {
 
-    public static int find(int x) {
-        if (x == parent[x]) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
+		x = find(x);
+		y = find(y);
 
-    public static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
+		if (x != y) { // 더 작은 쪽이 부모노드
+			if (x <= y)
+				parent[y] = x;
+			else
+				parent[x] = y;
+		}
 
-        if (x != y) {
-            if (x < y) parent[y] = x;
-            else parent[x] = y;
-        }
-    }
+	}
 
-    public static boolean isSameParent(int x, int y) {
-        return find(x) == find(y);
-    }
+	static int find(int x) {
+
+		if (parent[x] == x)
+			return x;
+
+		// 경로압축을 꼭 유의해야 한다.
+		// root노드가 항상 바로 다음단계의 부모노드가 되도록 한다.
+		return parent[x] = find(parent[x]);
+	}
+	
+	static boolean isSameParent(int a, int b) {
+		if(find(a) == find(b)) return true;
+		return false;
+	}
 }
