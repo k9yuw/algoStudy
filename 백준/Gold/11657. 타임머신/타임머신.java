92a2d945b72cf29529e053c82f94d8f61968f271
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2024-08-09 18:24:38 +0900

    [Gold IV] Title: 타임머신, Time: 240 ms, Memory: 18824 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -3,78 +3,86 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M;
-    static long[] costArr; 
-    final static long INF = 987654321;
-    
-    static class Edge {
-        int from, to, cost;
-        
-        public Edge(int from, int to, int cost){
-            this.from = from;
-            this.to = to;
-            this.cost = cost;
-        }
-    }
+	// 음수사이클의 최대값은 6000 * -10000 = -6천만이다.
+	// 그리고 전체 과정을 정점의 개수(500)만큼 돌면서 값을 키우기 때문에 500 * -6천만 만큼 절대값 크기가 늘게 된다.
+	// 따라서 dist 자료형을 long으로 선언해야 한다!
+	static final long INF = Integer.MAX_VALUE;
+	static int N, M;
+	static long[] minDist;
+	static boolean doesNegativeCycleExists = false;
+	static ArrayList<Edge> graph = new ArrayList<Edge>();
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		minDist = new long[N+1];
+		Arrays.fill(minDist, INF);
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			graph.add(new Edge(a, b, c));
+		}
+		
+		BellmanFord(N, M, 1);
+		getAns();
+		System.out.println(sb);
+	}
+	
+	static void BellmanFord(int n, int m, int start) {
+		
+		minDist[start] = 0;
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				Edge e = graph.get(j);
+				
+				if(minDist[e.from] != INF && minDist[e.to] > minDist[e.from] + e.cost) {
+					minDist[e.to] = minDist[e.from] + e.cost;
+				}
+			}
+		}
+		
+		for(int j=0; j<M; j++) {
+			Edge e = graph.get(j);
+			if(minDist[e.from] != INF && minDist[e.to] > minDist[e.from] + e.cost) {
+				doesNegativeCycleExists = true;
+			}
+		}
+		
+	}
+	
+	static void getAns() {
+			
+		if(doesNegativeCycleExists) {
+			sb.append("-1").append("\n");
+		} else {
+			for(int i=2; i<=N; i++) {
+				long result = minDist[i];
+				if(result == INF) {
+					sb.append("-1").append("\n");
+				} else {
+					sb.append(result).append("\n");
+				}
+			}
+		}
+	}
+	
+}
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        costArr = new long[N+1];  
-        List<Edge> edges = new ArrayList<>();
-
-        // 값 입력 받기
-        for (int i = 0; i<M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(x, y, cost));
-        }
-
-        
-        // 벨만-포드 알고리즘
-        // 최단거리에서 음수 간선, 등으로 인하여 계속해서 최단거리를 줄여나가는지 "감지" 해야하는 경우 사용
-        Arrays.fill(costArr, INF);
-        costArr[1] = 0;
-        
-        for(int i=2; i<=N; i++){
-            for(Edge edge: edges){
-                if(costArr[edge.from] != INF && costArr[edge.to] > costArr[edge.from] + edge.cost) {
-                    costArr[edge.to] = costArr[edge.from] + edge.cost;
-                }
-            }
-        }
-        
-        boolean hasNegativeCycle = false;
-        for(Edge edge: edges){
-            if(costArr[edge.from] != INF && costArr[edge.to] > costArr[edge.from] + edge.cost) { // 위에서 이거를 같게 만들어주는 작업을 했음에도 불구하고 여전히 차이가 나는 경우!!
-                    hasNegativeCycle = true;
-                    break; 
-                }
-        }
-        
-        
-        // 출력
-        if (hasNegativeCycle) {
-            bw.write("-1");
-        } else {
-            for (int i = 2; i <= N; i++) {
-                if (costArr[i] == INF) {
-                    bw.write("-1" + " ");
-                } else {
-                    bw.write(Long.toString(costArr[i]) + " "); 
-                }
-            }
-            bw.write("\n");
-        }
-
-        bw.flush();
-        bw.close();
-    }
+class Edge{
+	int from, to;
+	int cost;
+	public Edge(int from, int to, int cost) {
+		this.from = from;
+		this.to = to;
+		this.cost = cost;
+	}
 }
