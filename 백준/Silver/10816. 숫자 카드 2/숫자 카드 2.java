4dd8fbdec21e3c92e554742f5834d30ec6c0908a
Gyuwon Kim <124178101+k9yuw@users.noreply.github.com>
2025-05-12 18:30:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1304 ms, Memory: 122184 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,57 +3,64 @@ import java.util.*;
 
 public class Main {
 
-    static int n, m;
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < n; i++) {
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        m = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine()," ");
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < m; i++) {
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++) {
             int key = Integer.parseInt(st.nextToken());
-            // upperBound와 lowerBound의 차이 값이 카드의 개수
-            sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
+            int count = upperBound(key) - lowerBound(key);
+            sb.append(count).append(" ");
         }
+        
         System.out.println(sb);
     }
 
-    private static int lowerBound(int[] arr, int key) {
+    static int upperBound(int key) {
         int low = 0;
         int high = arr.length;
 
-        while (low < high) {
+        while(low < high) {
             int mid = (low + high) / 2;
-            if (key <= arr[mid]) high = mid;
-            else low = mid + 1;
+
+            if(key < arr[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
         return low;
     }
 
-    private static int upperBound(int[] arr, int key) {
+    static int lowerBound(int key) {
         int low = 0;
         int high = arr.length;
 
-        while (low < high) {
+        while(low < high) {
             int mid = (low + high) / 2;
-            if (key < arr[mid]) high = mid;
-            else low = mid + 1;
+            if(key <= arr[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
         return low;
     }
-
 }
